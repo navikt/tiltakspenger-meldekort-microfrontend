7c65323acc5d221a4b28795a4e8782bae283b4f2
Henrik Solum <henrik.solum@nav.no>
2025-08-20 13:25:41 +0200

    Initial commit

---
+++ src/language/text.ts
@@ -0,0 +1,12 @@
+export const text = {
+  title: {
+    nb: "tms-microfrontend-template-ssr (nb)",
+    en: "tms-microfrontend-template-ssr (en)",
+    nn: "tms-microfrontend-template-ssr (nn)",
+  },
+  name: {
+    nb: "Navn",
+    en: "Name",
+    nn: "Navn",
+  },
+};
