ce825197704ee7bcfabf0229b3653960465f0dff
Henrik Solum <henrik.solum@nav.no>
2025-08-29 13:28:28 +0200

    1346 Vis når man kan sende inn neste meldekort dersom man ikke har noen klare for innsending

--- src/language/text.ts
+++ src/language/text.ts
@@ -2,16 +2,21 @@ export const text = {
     tittel: {
         nb: "Meldekort for tiltakspenger",
         nn: "Meldekort for tiltakspengar",
-        en: "Benefit claim form for initiative money",
+        en: "Benefit activity report for participation allowance",
     },
     ett: {
         nb: "Send inn meldekort",
         nn: "Send inn meldekort",
-        en: "Submit benefit claim form",
+        en: "Submit activity report",
     },
     flere: {
         nb: "Du har {count} meldekort klare til innsending",
         nn: "Du har {count} meldekort klare til innsending",
-        en: "You have {count} benefit claim form ready to be sent",
+        en: "You have {count} activity reports ready to be sent",
+    },
+    neste: {
+        nb: "Neste meldekort kan sendes inn {dato}.",
+        nn: "Neste meldekort kan sendast inn {dato}.",
+        en: "The next activity report can be submitted {dato}.",
     },
 };
